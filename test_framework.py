7bdf1fef0964dee976910128f72c41b3e484e51a
Gustavo Henrique Modesto Vasconcelos <gustavohenrique011921@gmail.com>
2026-04-14 21:29:55 -0300

    8. Comandos Assert

--- test_framework.py
+++ test_framework.py
@@ -21,6 +21,21 @@ class TestCase:
     def tear_down(self):
         pass
 
+    def assert_equal(self, first, second):
+        if first != second:
+            msg = f'{first} != {second}'
+            raise AssertionError(msg)
+
+    def assert_true(self, expr):
+        if not expr:
+            msg = f'{expr} is not true'
+            raise AssertionError(msg)
+
+    def assert_false(self, expr):
+        if expr:
+            msg = f'{expr} is not false'
+            raise AssertionError(msg)
+
 class TestResult:
 
     RUN_MSG = 'run'
